1dadbecd805b9b740e2563dc5d2ba0eeb746fbee
zc <971016359@qq.com>
2017-04-04 01:44:58 +0800

    - 创建导航栏

--- DouYu/DouYu/Home /HomeViewController.swift
+++ DouYu/DouYu/Home /HomeViewController.swift
@@ -14,22 +14,49 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        setupUI()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//MARK:- 设置UI
+extension HomeViewController{
+    fileprivate func setupUI(){
+    
+        setupNavigationBar()
     }
-    */
-
+    
+    private func setupNavigationBar(){
+        navigationController?.navigationBar.barTintColor = UIColor.init(red: 254.0/255.0, green: 151.0/255.0, blue: 42.0/255.0, alpha: 1);
+        
+        //方法一
+        //        let logoImg = UIImage.init(named: "homeLogoIcon")?.withRenderingMode(.alwaysOriginal)
+        //
+        //        navigationItem.leftBarButtonItem = UIBarButtonItem.init(image: logoImg, style: .plain, target: self, action: nil)
+        
+        //方法二
+        let leftBtn = UIButton()
+        leftBtn.setImage(UIImage.init(named: "homeLogoIcon"), for: .normal)
+        leftBtn.sizeToFit()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftBtn)
+        
+        let btnSize = CGSize(width: 35, height: 25)
+        
+        let historyItem = creatNavigationItem(imageName: "viewHistoryIcon", size: btnSize)
+        let gameItem = creatNavigationItem(imageName: "home_newGameicon", size: btnSize)
+        
+        navigationItem.rightBarButtonItems = [historyItem, gameItem]
+    }
+    
+    private func creatNavigationItem(imageName: String, size: CGSize) -> UIBarButtonItem{
+        let btn = UIButton()
+        btn.setImage(UIImage(named: imageName), for: .normal)
+        btn.frame = CGRect(origin: CGPoint.zero, size: size)
+        let barButtonItem = UIBarButtonItem(customView: btn)
+        
+        return barButtonItem
+    }
 }
