de574102ecc7eb2d52ea6a431c90f3efc2b57dd4
zc <971016359@qq.com>
2017-04-03 14:16:41 +0800

    - 适配iOS8

--- DouYu/DouYu/main/MainTabViewController.swift
+++ DouYu/DouYu/main/MainTabViewController.swift
@@ -14,22 +14,15 @@ class MainTabViewController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let vcNameArr = ["Home", "Live", "Focus", "Discovery", "Mine"]
+        
+        for name in vcNameArr{
+            let homeVC = UIStoryboard.init(name: name, bundle: nil).instantiateInitialViewController()
+            
+            self.addChildViewController(homeVC!)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
